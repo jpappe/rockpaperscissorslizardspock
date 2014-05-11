51fcac8ff08c38d57ea1019f4f1dee1140da1a52
Jacob Pappe <jacob.pappe@datacash.com>
2014-05-11 10:19:28 +0100

    adding javadocs for methods (#6)

--- player-framework/src/main/java/com/jpappe/rockpaperscissors/framework/Player.java
+++ player-framework/src/main/java/com/jpappe/rockpaperscissors/framework/Player.java
@@ -8,7 +8,17 @@ package com.jpappe.rockpaperscissors.framework;
  */
 public interface Player {
 
+	/**
+	 * Ask the player for the Hand they want to play.
+	 * 
+	 * @return hand
+	 */
 	Hand getHand();
 
+	/**
+	 * Inform the user of the outcome of a round (i.e., who won, what hands were played, etc.).
+	 * 
+	 * @param outcome
+	 */
 	void reportOutcome( RoundOutcome outcome );
 }
